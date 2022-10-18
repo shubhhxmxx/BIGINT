e201c995ef8315abbdf107134fe602a5dc87d262
shubhhxmxx <80251094+shubhhxmxx@users.noreply.github.com>
2022-10-19 01:09:40 +0530

    Update HANDLINGBIGNUMBERS.cpp

--- HANDLINGBIGNUMBERS.cpp
+++ HANDLINGBIGNUMBERS.cpp
@@ -20,12 +20,12 @@ T sum(T a,Args... args) {return a+sum(args...);}
 class bigInt{
     public:
         string value;
-    bigint(string s){
+    bigint(string argument){
         bool f=0;
          try{
-            cin>>s;
-            for(auto x:s){
-                if((x>'9' || x<'0') && x!='-'){
+            cin>>argument;
+            for(auto ch:argument){
+                if((ch>'9' || ch<'0') && ch!='-'){
                     throw invalid_argument("Please enter only integer values");
                 }
             }
@@ -33,15 +33,15 @@ class bigInt{
             cerr<<e.what()<<endl;
             f=1;
         }
-        if(!f)value=s;
+        if(!f)value=argument;
     }
     void readVal(){
         cout<<"Enter number:";
-        string s;
+        string inputValue;
         try{
-            cin>>s;
-            for(auto x:s){
-                if((x>'9' || x<'0') && x!='-'){
+            cin>>inputValue;
+            for(auto ch:inputValue){
+                if((ch>'9' || ch<'0') && ch!='-'){
                     throw invalid_argument("Please enter only integer values");
                 }
             }
@@ -50,28 +50,29 @@ class bigInt{
             this->readVal();
             return;
         }
-        value=s;
+        value=inputValue;
     }
-    bool isZero(string a){
-        if(isnegative(a)) a=a.substr(1);
-        int n=a.size();
-        for(int i=0;i<n;i++){
-            if(a[i]!='0' && a[i]!='-') return 0;
+    bool isnegative(string inputValue){
+        if(inputValue[0]=='-') return 1;
+        return 0;
+    }
+
+    bool isZero(string inputValue){
+        if(isnegative(inputValue)) inputValue=inputValue.substr(1);
+        int length=inputValue.size();
+        for(int i=0;i<length;i++){
+            if(inputValue[i]!='0' && inputValue[i]!='-') return 0;
         }
         return 1;
     }
-    bool isnegative(string s){
-        if(s[0]=='-') return 1;
-        return 0;
-    }
-    int comparehelper(string a,string b){
-        int p1=a.size();
-        int p2=b.size();
+    int comparehelper(string value1,string value2){
+        int p1=value1.size();
+        int p2=value2.size();
         if(p1>p2) return 1;
         if(p1<p2) return -1;
         int i=0,diff=0,bigger=0;
         do{
-         diff=a[i]-b[i];
+         diff=value1[i]-value2[i];
          if(diff>0) bigger=1;
          else if(diff<0) bigger=-1;
          ++i;
@@ -79,37 +80,36 @@ class bigInt{
         return bigger;
     }
     int compareBigInt(bigInt B){
-        string a=value;
-        string b=B.value;
-        if(isnegative(a)) a=a.substr(1);
-        if(isnegative(b)) b=b.substr(1);
-        return comparehelper(a,b);
+        string value1=value;
+        string value2=B.value;
+        if(isnegative(value1)) value1=value1.substr(1);
+        if(isnegative(value2)) value2=value2.substr(1);
+        return comparehelper(value1,value2);
     }
 
-    string removeZeroes(string res){
-        int nonz=0,newlen=0,i=0;
-        int len=res.size()-1;
-        for(nonz=0;nonz<res.size();nonz++){
-            if(res[nonz]!='0') break;
+    string removeZeroes(string inputValue){
+        int nonz=0,newlength=0,i=0;
+        int length=inputValue.size()-1;
+        for(nonz=0;nonz<inputValue.size();nonz++){
+            if(inputValue[nonz]!='0') break;
         }
-        if(nonz==0) return res;
-        res=res.substr(nonz);
+        if(nonz==0) return inputValue;
+        inputValue=inputValue.substr(nonz);
 
-        return res;
-
+        return inputValue;
     }
 
     string adderBigInt(bigInt B){
-        string a=this->value;
-        string b=B.value;
-        int p1=a.size()-1;
-        int p2=b.size()-1;
+        string value1=this->value;
+        string value2=B.value;
+        int p1=value1.size()-1;
+        int p2=value2.size()-1;
         int sum=0,carry=0,k=p1+1;
-        string c;
-        for(int i=0;i<=k;i++) c+='0';
+        string ans;
+        for(int i=0;i<=k;i++) ans+='0';
         while(p2>=0){
-            sum=a[p1]+b[p2]+carry-'0'-'0';
-            c[k]='0'+sum%10;
+            sum=value1[p1]+value2[p2]+carry-'0'-'0';
+            ans[k]='0'+sum%10;
             carry=sum/10;
             p1--;
             p2--;
@@ -117,78 +117,78 @@ class bigInt{
 
         }
         while(p1>=0){
-            sum=a[p1]+carry-'0';
-            c[k]='0'+sum%10;
+            sum=value1[p1]+carry-'0';
+            ans[k]='0'+sum%10;
             carry=sum/10;
             p1--;
             k--;
         }
         if(carry){
-            c[k]='0'+carry;
+            ans[k]='0'+carry;
         }
-        c=removeZeroes(c);
-        return c ;
+        ans=removeZeroes(ans);
+        return  ans;
     }
-    string multiplierBigInt(string a,string b){
-        if(isnegative(a)) a=a.substr(1);
-        if(isnegative(b)) b=b.substr(1);
-        int l1=a.size();
-        int l2=b.size();
+    string multiplierBigInt(string inputValue1,string inputValue2){
+        if(isnegative(inputValue1)) inputValue1=inputValue1.substr(1);
+        if(isnegative(inputValue2)) inputValue2=inputValue2.substr(1);
+        int l1=inputValue1.size();
+        int l2=inputValue2.size();
         int sum=0,carry=0;
         int l3=l1+l2;
         int ptr1=0;
         int ptr2;
         int i;
         //error(a,b);
-        string res;
-        for(int i=0;i<=l3;i++) res+='0';
+        string ans;
+        for(int i=0;i<=l3;i++) ans+='0';
         for(i=l2-1;i>=0;i--){
             sum=0,carry=0;
             ptr2=l3-ptr1;
             for(int j=l1-1;j>=0;j--){
-                sum=(a[j]-'0')*(b[i]-'0')+(res[ptr2]-'0')+carry;
-                res[ptr2]='0'+sum%10;
+                sum=(inputValue1[j]-'0')*(inputValue2[i]-'0')+(ans[ptr2]-'0')+carry;
+                ans[ptr2]='0'+sum%10;
               //  ////error(sum);
                 carry=sum/10;
                 --ptr2;
 
             }
-            if(carry) res[ptr2]+=carry;
+            if(carry) ans[ptr2]+=carry;
         ++ptr1;
         }
         //cout<<res<<endl;
-        res=removeZeroes(res);
-        return res;
+        ans=removeZeroes(ans);
+        return ans;
 }
-    string subtracterBigInt(bigInt B){
-        string b=B.value;
-        string a=this->value;
+string subtracterBigInt(bigInt B){
+        string inputValue2=B.value;
+        string inputValue1=this->value;
         int p=this->compareBigInt(B);
-        if(p<0) {swap(a,b);
+        if(p<0) {swap(inputValue1,inputValue2);
         p*=-1;}
-        int p1=a.size()-1;
-        int p2=b.size()-1;
-        string c;
+        int p1=inputValue1.size()-1;
+        int p2=inputValue2.size()-1;
+        string ans;
         int taken=0,k=p1;
-        for(int i=0;i<=p1;i++) c+='0';
+        for(int i=0;i<=p1;i++) ans+='0';
         while(p2>=0){
-            int top=a[p1]-'0';
-            int bottom=b[p2]-'0';
+            int top=inputValue1[p1]-'0';
+            int bottom=inputValue2[p2]-'0';
             if(taken>0){
                 if(taken==1){
                     top-=1;
                 }else{
-                top=9+a[p1]-'0';
+                top=9+inputValue1[p1]-'0';
                 }
                 --taken;
             }
             if(top<bottom){
                 do{
                     ++taken;
-                }while(a[p1-taken]=='0' && (p1-taken)>0);
+                }while(inputValue1[p1-taken]=='0' && (p1-taken)>0);
                 top+=10;
             }
-            c[k]='0'+top-bottom;
+            ans[k]='0'+top-bottom;
             p1--;
             p2--;
             k--;
@@ -196,64 +196,65 @@ class bigInt{
         }
         while(p1>=0){
             if(taken>0){
-                c[k]=a[p1]-1;
-                if(a[p1]=='0')
-                    c[k]='9';
+                ans[k]=inputValue1[p1]-1;
+                if(inputValue1[p1]=='0')
+                    ans[k]='9';
                 --taken;
 
             }
                 else{
-                    c[k]=a[p1];
+                    ans[k]=inputValue1[p1];
                 }
                 --p1;
                 --k;
 
         }
         //cout<<c<<endl;
-        c=removeZeroes(c);
-        return c;
+        ans=removeZeroes(ans);
+        return ans;
     }
+
     string multiplierBigInt(bigInt B){
-        string b=B.value;
-        string a=this->value;
-        if(isnegative(a)) a=a.substr(1);
-        if(isnegative(b)) b=b.substr(1);
-        int l1=a.size();
-        int l2=b.size();
+        string inputValue2=B.value;
+        string inputValue1=this->value;
+        if(isnegative(inputValue1)) inputValue1=inputValue1.substr(1);
+        if(isnegative(inputValue2)) inputValue2=inputValue2.substr(1);
+        int l1=inputValue1.size();
+        int l2=inputValue2.size();
         int sum=0,carry=0;
         int l3=l1+l2;
         int ptr1=0;
         int ptr2;
         int i;
         ////error(a,b);
-        string res;
-        for(int i=0;i<=l3;i++) res+='0';
+        string ans;
+        for(int i=0;i<=l3;i++) ans+='0';
         for(i=l2-1;i>=0;i--){
             sum=0,carry=0;
             ptr2=l3-ptr1;
             for(int j=l1-1;j>=0;j--){
-                sum=(a[j]-'0')*(b[i]-'0')+(res[ptr2]-'0')+carry;
-                res[ptr2]='0'+sum%10;
+                sum=(inputValue1[j]-'0')*(inputValue2[i]-'0')+(ans[ptr2]-'0')+carry;
+                ans[ptr2]='0'+sum%10;
               //  ////error(sum);
                 carry=sum/10;
                 --ptr2;
 
             }
-            if(carry) res[ptr2]+=carry;
+            if(carry) ans[ptr2]+=carry;
         ++ptr1;
         }
         //cout<<res<<endl;
-        res=removeZeroes(res);
-        return res;
+        ans=removeZeroes(ans);
+        return ans;
     }
 
     void subtractBigInt(bigInt B){
-        string a=this->value;
-        string b=B.value;
-        bool f1=isnegative(a);
-        bool f2=isnegative(b);
+        string inputValue1=this->value;
+        string inputValue2=B.value;
+        bool f1=isnegative(inputValue1);
+        bool f2=isnegative(inputValue2);
         int p=this->compareBigInt(B);
-        if(isZero(a) && isZero(b)){
+        if(isZero(inputValue1) && isZero(inputValue2)){
             value="0";
             return ;
         }
@@ -266,53 +267,53 @@ class bigInt{
         */
         if(f1==f2){
             //subtract
-            if(f1) a=a.substr(1);
-            if(f2) b=b.substr(1);
-            bigInt C({a});
-            bigInt D({b});
-            string res=C.subtracterBigInt(D);
+            if(f1) inputValue1=inputValue1.substr(1);
+            if(f2) inputValue2=inputValue2.substr(1);
+            bigInt C({inputValue1});
+            bigInt D({inputValue2});
+            string ans=C.subtracterBigInt(D);
             //error(a,b,res);
-            if((!f2 && p<0)||(f1 && p>0)) res='-'+res;
-            if(isZero(res)) res="0";
-            this->value=res;
+            if((!f2 && p<0)||(f1 && p>0)) ans='-'+ans;
+            if(isZero(ans)) ans="0";
+            this->value=ans;
             return;
         }
         else{
             //add
-            if(f1) a=a.substr(1);
-            else b=b.substr(1);
-            if(p<0) swap(a,b);
-            bigInt C({a});
-            bigInt D({b});
+            if(f1) inputValue1=inputValue1.substr(1);
+            else inputValue2=inputValue2.substr(1);
+            if(p<0) swap(inputValue1,inputValue1);
+            bigInt C({inputValue1});
+            bigInt D({inputValue2});
            // error(C.value,D.value);
-            string res=C.adderBigInt(D);
+            string ans=C.adderBigInt(D);
             //error(res);
-            if(f1) res='-'+res;
-            value=res;
+            if(f1) ans='-'+ans;
+            value=ans;
             return;
             }
 
 
     }
     void addBigInt(bigInt B){
-        string a=value;
-        string b=B.value;
+        string inputValue1=value;
+        string inputValue2=B.value;
         int p=this->compareBigInt(B);
-        bool f1=isnegative(a);
-        bool f2=isnegative(b);
-        if(isZero(a) && isZero(b)) {
+        bool f1=isnegative(inputValue1);
+        bool f2=isnegative(inputValue2);
+        if(isZero(inputValue1) && isZero(inputValue2)) {
             value="0";
             return;
         }
         if(f1^f2==1){
             int p2=this->compareBigInt(B);
             if(f1){
-                a=a.substr(1);
+                inputValue1=inputValue1.substr(1);
                 }
             else{
-                b=b.substr(1);
+                inputValue2=inputValue2.substr(1);
                 }
-            if(p2<0) swap(a,b);
+            if(p2<0) swap(inputValue1,inputValue2);
             /*
             subtract
             +-{
@@ -323,56 +324,56 @@ class bigInt{
                 +  ans+
                 -  ans-
             */
-            bigInt C({a}),D({b});
+            bigInt C({inputValue1}),D({inputValue2});
             C.subtractBigInt(D);
-            string res=C.value;
-            if((f1 && p2>0)||(f2&&p2<0)) res='-'+res;
-            value=res;
+            string ans=C.value;
+            if((f1 && p2>0)||(f2&&p2<0)) ans='-'+ans;
+            value=ans;
             return ;
         }
         else{
-        string res;
+        string ans;
             if(f1){
-                a=a.substr(1);
-                b=b.substr(1);
+                inputValue1=inputValue1.substr(1);
+                inputValue2=inputValue2.substr(1);
              }
-               bigInt C({b});
-               bigInt D({a});
+               bigInt C({inputValue2});
+               bigInt D({inputValue1});
 
              if(p<0){
-                    res=C.adderBigInt(D);
+                    ans=C.adderBigInt(D);
 
                     }
                 else{
-                     res=D.adderBigInt(C);
+                     ans=D.adderBigInt(C);
                 }
-             if(f1==1) res='-'+res;
-        this->value=res;
+             if(f1==1) ans='-'+ans;
+        this->value=ans;
         return;
         }
 
 
     }
     void multBigInt(bigInt B){
-    string a=this->value;
-    string b=B.value;
-    if(isZero(a)|| isZero(b)){
+    string inputValue1=this->value;
+    string inputValue2=B.value;
+    if(isZero(inputValue1)|| isZero(inputValue2)){
         this->value="0";
         return;
     }
     else {
-            string res;
-            if(isnegative(a)^isnegative(b)){
-                res=multiplierBigInt(a,b);
-                res='-'+res;
-                this->value=res;
+            string ans;
+            if(isnegative(inputValue1)^isnegative(inputValue2)){
+                ans=multiplierBigInt(inputValue1,inputValue2);
+                ans='-'+ans;
+                this->value=ans;
                 return;
             }
             else {
-                    res=multiplierBigInt(a,b);
+                    ans=multiplierBigInt(inputValue1,inputValue2);
                 }
 
-            this->value=res;
+            this->value=ans;
             return ;
 
         }
@@ -382,22 +383,22 @@ class bigInt{
 
 };
 bigInt fib(int n){
-    bigInt a({"0"}),b({"1"}),c;
+    bigInt value1({"0"}),value2({"1"}),value3;
     for(int i=0;i<n-2;i++){
-        a.addBigInt(b);
-        c.value=a.value;
-        a.value=b.value;
-        b.value=c.value;
+        value1.addBigInt(value2);
+        value3.value=value1.value;
+        value1.value=value2.value;
+        value2.value=value3.value;
     }
-    return c;
+    return value3;
 }
 bigInt fact(int n){
-    bigInt a({"1"}),ans({"1"}),incerement({"1"});
-    bigInt b({to_string(n)});
+    bigInt value1({"1"}),ans({"1"}),incerement({"1"});
+    bigInt value2({to_string(n)});
  //   cout<<b.value<<endl;
-    while(a.compareBigInt(b)!=0){
-        ans.multBigInt(a);
-        a.addBigInt(incerement);
+    while(value1.compareBigInt(value2)!=0){
+        ans.multBigInt(value1);
+        value1.addBigInt(incerement);
     }
     return ans;
 }
